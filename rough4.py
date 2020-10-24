f2ef57ea2cca68154782996fc7c0f296c01c841b
ansh98-crypto <61378952+ansh98-crypto@users.noreply.github.com>
2020-10-24 23:43:35 +0530

    Add files via upload

---
+++ rough4.py
@@ -0,0 +1,59 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
+import socket
+import select
+import errno
+import sys
+import os
+import subprocess
+from threading import Thread
+
+
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.cols = 2
+        #self.inside = GridLayout()
+        #self.inside.cols = 2
+
+        self.add_widget(Label(text='Message: '))
+        self.stats = TextInput(multiline=False)
+        self.add_widget(self.stats)
+        #self.add_widget(self.inside)
+
+        self.enter = Button(text='Start Connection')  # , font_size=40)
+        self.enter.bind(on_press=self.establish_connection)
+        self.add_widget(Label())
+        self.add_widget(self.enter)
+
+
+    def recv_message(self):
+        while True:
+            self.stats.text = str(self.socket.recv(120002).decode("utf-8"))
+             
+
+    def establish_connection(self, button):
+        self.socket = socket.socket()
+        self.host = '192.168.1.3'
+        self.port = 9999
+        self.socket.connect((self.host, self.port))
+        Thread(target=self.recv_message).start()
+
+    def recv_message2(self):
+        while True:
+            self.stats.text = self.socket.recv_message()
+
+
+class Myapp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == '__main__':
+    Myapp().run()
